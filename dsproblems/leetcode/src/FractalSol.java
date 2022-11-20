2b041a903aebb1600a2408367379d3c12e7d9530
Gupta <roshan.kumar.gupta@sap.com>
2022-11-20 15:29:32 +0530

    leetcode sol

---
+++ dsproblems/leetcode/src/FractalSol.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FractalSol {
+
+    
+
+    public static void main(String args[]) throws Exception {
+        System.out.println("hello");
+    }
+}
